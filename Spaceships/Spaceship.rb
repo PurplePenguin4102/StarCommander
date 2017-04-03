8e640eff1280f2086ff14be1b7a3abd42751ea77
Joey Ray <joseph.h.ray@gmail.com>
2017-04-04 09:20:56 +1000

    moving spaceship into its own folder

---
+++ Spaceships/Spaceship.rb
@@ -0,0 +1,29 @@
+class Spaceship < StellarObject
+	attr_accessor :vx, :vy, :vz
+	attr_accessor :thrusters
+	attr_accessor :sails
+	attr_accessor :captain
+	attr_accessor :marines
+	attr_accessor :crew
+	attr_accessor :personnel
+	attr_accessor :shields
+
+	def increase_yaw(amount)
+
+	end
+
+	def increase_pitch(amount)
+
+	end
+
+	def increase_roll(amount)
+
+	end
+
+	def speed?
+	end
+
+	def weight?
+	end
+end
+
