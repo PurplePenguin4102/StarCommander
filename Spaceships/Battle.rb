59cc8b94c666b2e7f32cf8b791e662536a01435a
Joey Ray <joseph.h.ray@gmail.com>
2017-04-05 21:26:45 +1000

    Update Battle.rb

--- Spaceships/Battle.rb
+++ Spaceships/Battle.rb
@@ -2,7 +2,11 @@ class Battle
 	attr_accessor :ships
 
 	def initialize(ships)
-		ships.each {|ship| ship.y = rand(100)}
+		ships.each do |ship| 
+			ship.y = rand(100)
+			ship.x = rand(100)
+			ship.z = rand(100)
+		end
 		@ships = ships
 	end
 
