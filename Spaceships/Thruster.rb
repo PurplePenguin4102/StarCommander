166f1b980876ca69a32f410bf27f5d3da2a645ed
Joey Ray <joseph.h.ray@gmail.com>
2017-04-08 20:12:08 +1000

    Create Thruster.rb

---
+++ Spaceships/Thruster.rb
@@ -0,0 +1,12 @@
+require './Requirements'
+
+class Thruster
+  attr_accessor :tech_level
+  attr_accessor :friendly_name
+  attr_accessor :description
+  attr_accessor :thrust_ability #in MW
+  
+  
+  def initialize
+  end
+end
